8092086f1c91ac8ef141d06ddcedb55d4bafd966
Sean Ezenwa <seanezenwa@gmail.com>
2023-05-06 14:02:44 -0400

    Finish submit logic

--- cv-project/src/components/generalInfo.js
+++ cv-project/src/components/generalInfo.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
 
 class GeneralInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            submitted: false,
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+            description: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({submitted: true});
+    }
+
+    handleChange = (event) => {
+        const {name, value}= event.target
+        this.setState({[name]: value});
+    }
 
     render() {
+        const { submitted, firstName, lastName, email, phoneNumber, description } = this.state;
+        if(submitted) {
+            return (
+                <div className="module">
+                  <h1>General Information</h1>
+                  <p>Name: {firstName} {lastName}</p>
+                  <p>Email: {email}</p>
+                  <p>Phone Number: {phoneNumber}</p>
+                  <p>Description: {description}</p>
+                  <button onClick={() => this.setState({ submitted: false })}>EDIT</button>
+                </div>
+            );
+        }
+
         return (
             <div className="module">
                 <h1>General Information</h1>
-                <form>
-                    <input type="text" name="firstName" placeholder="First Name"></input>
-                    <input type="text" name="lastName" placeholder="Last Name"></input>
-                    <input type="text" name="email" placeholder="Email"></input>
-                    <input type="text" name="phoneNumber" placeholder="Phone Number"></input>
-                    <input type="text" name="description" placeholder="Description"></input>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange}></input>
+                    <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange}></input>
+                    <input type="text" name="email" placeholder="Email" onChange={this.handleChange}></input>
+                    <input type="text" name="phoneNumber" placeholder="Phone Number" onChange={this.handleChange}></input>
+                    <input type="text" name="description" placeholder="Description" onChange={this.handleChange}></input>
                     <div>
                         <button>EDIT</button>
                         <button type="submit">SUBMIT</button>
--- cv-project/src/components/practicalExperience.js
+++ cv-project/src/components/practicalExperience.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react'
 
 class PracticalExperience extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            submitted: false,
+            companyName: '',
+            positionTitle: '',
+            city: '',
+            from: '',
+            to: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({submitted: true});
+    }
+
+    handleChange = (event) => {
+        const {name, value}= event.target
+        this.setState({[name]: value});
+    }
 
     render() {
+        const { submitted, companyName, positionTitle, city, from, to } = this.state;
+        if(submitted) {
+            return (
+                <div className="module">
+                  <h1>Practical Experience</h1>
+                  <p>Company Name: {companyName}</p>
+                  <p>Position Title: {positionTitle}</p>
+                  <p>City: {city}</p>
+                  <p>From: {from}</p>
+                  <p>To: {to}</p>
+                  <button onClick={() => this.setState({ submitted: false })}>EDIT</button>
+                </div>
+            );
+        }
+
         return (
             <div className="module">
                 <h1>Practical Experience</h1>
-                <form>
-                    <input type="text" name="companyName" placeholder="Company Name"></input>
-                    <input type="text" name="positionTitle" placeholder="Position Title"></input>
-                    <input type="text" name="city" placeholder="City"></input>
-                    <input type="text" name="fromDate" placeholder="From"></input>
-                    <input type="text" name="toDate" placeholder="To"></input>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="companyName" placeholder="Company Name" onChange={this.handleChange}></input>
+                    <input type="text" name="positionTitle" placeholder="Position Title" onChange={this.handleChange}></input>
+                    <input type="text" name="city" placeholder="City" onChange={this.handleChange}></input>
+                    <input type="text" name="fromDate" placeholder="From" onChange={this.handleChange}></input>
+                    <input type="text" name="toDate" placeholder="To" onChange={this.handleChange}></input>
                     <div>
                         <button>EDIT</button>
                         <button type="submit">SUBMIT</button>
